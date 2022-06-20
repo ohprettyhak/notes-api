5ed96603a7bed733d43ed5e670bf7e9b93956480
ohprettyhak <haklee.googl@gmail.com>
2022-06-20 23:41:52 +0900

    fix: CORS error

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,12 @@
 import * as functions from 'firebase-functions';
 import express from 'express';
+import cors from 'cors';
 
 // routes
 import spotify from './routes/spotify';
 
 const app = express();
+app.use(cors());
 
 app.use('/spotify', spotify);
 
